abf5805be458a7292c2401ea20a2b218bb93e852
Gabriela <gabisato.ssb@gmail.com>
2019-07-08 13:01:35 -0300

    card responsivo para mobile

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,36 +1,25 @@
-import React from "react";
+import React from 'react';
+import styles from './styles.module.css';
 
 const Card = ({ title, description }) => {
-  return (
-    <div style={{ display: "flex", flexDirection: "column", width: "100%", padding: '22px 11px', border: '1px solid #d8d8d8' }}>
-      <div style={{ display: "flex", flex: 2, flexDirection: "column", justifyContent: 'center', alignItems: 'center' }}>
-        <figure className="image is-128x128">
-          <img
-            src="https://bulma.io/images/placeholders/256x256.png"
-            alt=""
-            className="is-rounded"
-          />
-        </figure>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flex: 1,
-          flexDirection: "column",
-          textAlign: "center",
-          padding: '22px 0'
-        }}
-      >
-        <h3>Título</h3>
-        <p>Descrição</p>
-      </div>
-      <div style={{ display: "flex", flex: 1, flexDirection: "column" }}>
-        <a href="#" className="button">
-          Saiba mais
-        </a>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.card}>
+			<div className={styles.image}>
+				<figure className="image is-128x128">
+					<img src="https://bulma.io/images/placeholders/256x256.png" alt="" className="is-rounded" />
+				</figure>
+			</div>
+			<div className={styles.content}>
+				<h3>Título</h3>
+				<p>Descrição</p>
+			</div>
+			<div className={styles.footer}>
+				<a href="#" className="button">
+					Saiba mais
+				</a>
+			</div>
+		</div>
+	);
 };
 
 export default Card;
---
+++ src/view/Main.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Card from '../components/Card';
+import styles from './styles.module.css';
+
+const Main = () => {
+	return (
+		<React.Fragment>
+			<Navbar />
+			<section className="hero">
+				<div className="hero-body">
+					<div className="container">
+						<h1 className="title">Circuito Café</h1>
+					</div>
+				</div>
+			</section>
+			<section className="section">
+				<div className="container">
+					<div className={styles.row}>
+						<Card />
+						<Card />
+						<Card />
+					</div>
+				</div>
+			</section>
+			<section className="section">
+				<div className="container" />
+			</section>
+			<Footer />
+		</React.Fragment>
+	);
+};
+
+export default Main;
