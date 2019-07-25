b892562cce906a7e5ced719bb264626c2d87d1a7
Gabriela <gabisato.ssb@gmail.com>
2019-07-25 12:45:56 -0300

    Updates

---
+++ precache-manifest.3068f2a7a54eea6ddd9cc19ea2368443.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f742dffaf7c3fdccb65e5b3dc3149919",
+    "url": "/uticjr/alquimista_cafe_homepage/index.html"
+  },
+  {
+    "revision": "948f1be4f1eeedb5a7c4",
+    "url": "/uticjr/alquimista_cafe_homepage/static/css/2.fdfb8000.chunk.css"
+  },
+  {
+    "revision": "2a0e9eb8cbe9e1bf7121",
+    "url": "/uticjr/alquimista_cafe_homepage/static/css/main.07a0ccd8.chunk.css"
+  },
+  {
+    "revision": "948f1be4f1eeedb5a7c4",
+    "url": "/uticjr/alquimista_cafe_homepage/static/js/2.046b0f80.chunk.js"
+  },
+  {
+    "revision": "2a0e9eb8cbe9e1bf7121",
+    "url": "/uticjr/alquimista_cafe_homepage/static/js/main.f9a7157c.chunk.js"
+  },
+  {
+    "revision": "a1c50b6eef54372b5db1",
+    "url": "/uticjr/alquimista_cafe_homepage/static/js/runtime~main.72c70802.js"
+  },
+  {
+    "revision": "429a1f8cc5123de52300081fa9514a2d",
+    "url": "/uticjr/alquimista_cafe_homepage/static/media/5.429a1f8c.png"
+  },
+  {
+    "revision": "35889d8a5f1ccdbc3dd85a4c1afd1a21",
+    "url": "/uticjr/alquimista_cafe_homepage/static/media/KlinicSlabBold.35889d8a.otf"
+  },
+  {
+    "revision": "2d3ef9041dd7e174d170e95e30d2076b",
+    "url": "/uticjr/alquimista_cafe_homepage/static/media/KlinicSlabBook.2d3ef904.otf"
+  },
+  {
+    "revision": "82c07b75613bec35ee3689d5d00bb91a",
+    "url": "/uticjr/alquimista_cafe_homepage/static/media/inverted_sf.82c07b75.png"
+  },
+  {
+    "revision": "9b158dd779f0dbbebe61b17d831103e8",
+    "url": "/uticjr/alquimista_cafe_homepage/static/media/logo.9b158dd7.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/uticjr/alquimista_cafe_homepage/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/uticjr/alquimista_cafe_homepage/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/uticjr/alquimista_cafe_homepage/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/uticjr/alquimista_cafe_homepage/static/media/slick.f97e3bbf.svg"
+  }
+]);
