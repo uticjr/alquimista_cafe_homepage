c3cfcf4893388a7221bc4ac0370f29fa0e435f65
Gabriela <gabisato.ssb@gmail.com>
2019-07-31 02:56:28 -0300

    build pro deploy do site

---
+++ build/precache-manifest.981c76e5f755b264a9827826a1e894e6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a5a6a82e99daf5b6eb879d9b9f57ca8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "852465581a6a153c8033",
+    "url": "/static/css/2.18585a14.chunk.css"
+  },
+  {
+    "revision": "20822aa9c31a8abe385f",
+    "url": "/static/css/main.798c29db.chunk.css"
+  },
+  {
+    "revision": "852465581a6a153c8033",
+    "url": "/static/js/2.046b0f80.chunk.js"
+  },
+  {
+    "revision": "20822aa9c31a8abe385f",
+    "url": "/static/js/main.ea516d07.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "429a1f8cc5123de52300081fa9514a2d",
+    "url": "/static/media/5.429a1f8c.png"
+  },
+  {
+    "revision": "35889d8a5f1ccdbc3dd85a4c1afd1a21",
+    "url": "/static/media/KlinicSlabBold.35889d8a.otf"
+  },
+  {
+    "revision": "2d3ef9041dd7e174d170e95e30d2076b",
+    "url": "/static/media/KlinicSlabBook.2d3ef904.otf"
+  },
+  {
+    "revision": "82c07b75613bec35ee3689d5d00bb91a",
+    "url": "/static/media/inverted_sf.82c07b75.png"
+  },
+  {
+    "revision": "9b158dd779f0dbbebe61b17d831103e8",
+    "url": "/static/media/logo.9b158dd7.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
